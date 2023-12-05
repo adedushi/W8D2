2235422689ed0520828379c668db6871f50d8c36
Albert Dedushi <adedushi@hotmail.com>
2023-12-05 16:20:10 -0500

    Finish Array.prototype.myMap

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,5 +1,3 @@
-// Your code here
-
 Array.prototype.myEach = function(callback) {
     for(let i = 0; i < this.length; i++) {
         callback(this[i]);
@@ -7,20 +5,42 @@ Array.prototype.myEach = function(callback) {
 }
 
 
+// function callback(el) {
+//     console.log(el + 1);
+// }
+
+// [1,2,3].myEach(callback)
+
+// Array.prototype.myMap = function(callback2) {
+//     res = [];
+//     res += this.myEach(callback2)
+//     return res;
+// }
+
+// function callback2(el) {
+//     return el + 1
+// }
+
+// [1,2,3].myMap(callback2)
+
+Array.prototype.myMap = function(callback) {
+    let arr = [];
+
+    this.myEach(function(el){
+        arr.push(callback(el));
+    });
+
+    return arr;
+}
+
+
 function callback(el) {
     console.log(el + 1);
 }
 
-[1,2,3].myEach(callback)
+[1,2,3].myMap(callback)
 
-Array.prototype.myMap = function(callback2) {
-    res = [];
-    res += this.myEach(callback2)
-    return res;
-}
 
-function callback2(el) {
-    return el + 1
-}
-
-[1,2,3].myMap(callback2)
+// Array.prototype.myReduce(callback[, initialValue]) = {
+    
+// }
