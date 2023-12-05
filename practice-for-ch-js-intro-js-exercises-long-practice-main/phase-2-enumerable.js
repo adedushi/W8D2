8d73e5ead219e82c50e184b465de66a9ff6dd346
Peter Nolan <peter.l.nolan@gmail.com>
2023-12-05 15:12:59 -0500

    Finish Array.prototype.MyEach

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,26 @@
 // Your code here
+
+Array.prototype.myEach = function(callback) {
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+}
+
+
+function callback(el) {
+    console.log(el + 1);
+}
+
+[1,2,3].myEach(callback)
+
+Array.prototype.myMap = function(callback2) {
+    res = [];
+    res += this.myEach(callback2)
+    return res;
+}
+
+function callback2(el) {
+    return el + 1
+}
+
+[1,2,3].myMap(callback2)
